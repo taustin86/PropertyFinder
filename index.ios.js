4adab6fcb8d4023f3059a0ab641f51727ee7e85b
Tamara Austin <tamara.austin08@gmail.com>
2017-04-02 16:36:52 -0400

    Initial commit property finder app

---
+++ index.ios.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var React = require('react');
+var ReactNative = require('react-native');
+var SearchPage = require('./SearchPage');
+
+var styles = ReactNative.StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
+
+class PropertyFinderApp extends React.Component {
+  render() {
+    return (
+      <ReactNative.NavigatorIOS style={styles.container} initialRoute={{title: 'Property Finder', component: SearchPage}} />
+    );
+  }
+}
+
+ReactNative.AppRegistry.registerComponent('PropertyFinder', function() { return PropertyFinderApp });
